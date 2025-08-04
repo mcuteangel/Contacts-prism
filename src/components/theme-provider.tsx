bcd8af2d085f7da8b7d701dd363e7fac4f727f8d
mcuteangel <mpcuteangel@Gmail.com>
2025-08-04 14:35:13 +0330

    Reverted all changes back to version 0072f46498c1203c1ea7320fb80a38c2b0709eeb

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import { type ThemeProviderProps } from "next-themes" // Corrected import path
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
